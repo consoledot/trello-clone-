fb9b222c5a93c6fb2601e890476c0da0d23be2a8
bimbooladele@outlook.com <abimbola.oladele@thebulb.africa>
2020-12-19 19:10:22 +0100

    added new styles

--- src/styles.ts
+++ src/styles.ts
@@ -11,7 +11,7 @@ export const AppContainer = styled.div`
     height:100%;
 `
 export const ColumnContainer = styled.div`
-    background-color: #ebecfe0;
+    background-color: #ebecf0;
     width: 300px;
     min-height: 40px;
     margin-right: 20px;
@@ -32,4 +32,47 @@ export const CardContainer = styled.div`
     max-width: 300px;
     border-radius: 3px;
     box-shadow: #091e4240 0px 1px 0px 0px;
+`
+interface AdddItemButtonProps{
+    dark?:boolean
+}
+export const AddItemButton = styled.button<AdddItemButtonProps>`
+    background-color: #ffffff3d;
+    border-radius:3px;
+    border:none;
+    color:${props=> (props.dark ? "#000": "#fff")};
+    crsor:pointer;
+    max-width:300px;
+    padding:10px 12px;
+    text-align:left;
+    transition:background 85ms ease-in;
+    width:100%;
+    &:hover{
+        background-color:#ffffff52;
+    }
+`
+
+export const NewItemFormContainer = styled.div`
+    max-width:300px;
+    display:flex;
+    flex-direction:column;
+    width:100%;
+    align-items: flex-start;
+`
+export const NewItemButton = styled.button`
+    background-color: #5aac44;
+    border-radius:3px;
+    border:none;
+    box-shadow:none;
+    color:#fff;
+    padding:6px 12px;
+    text-align:center;
+`
+export const NewItemInput = styled.input`
+    border-radius: 3px;
+    border: none;
+    box-shadow: #091e4240 0px 1px 0px 0px;
+    margin-buttom: 0.5rem;
+    padding: 0.5rem;
+    width; 100%;
 `
